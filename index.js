93883ec9c7f6798c483f35a23f5a68cb3a9a5543
Alex Purcell <alextpurcell05@gmail.com>
2021-06-11 16:39:29 -0600

    Setup Express and Routes

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello");
+var path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.engine(".html", require("ejs").__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+
+// GET http://localhost:3000/home/?name=alex
+app.get("/", (req, res) => {
+  // go look something up in the database
+  res.render("index");
+  //res.send("<html><body>alex</body></html>");
+});
+
+app.get("/milestones", (req, res) => {
+  // go look something up in the database
+  res.render("milestones");
+  //res.send("<html><body>alex</body></html>");
+});
+
+app.get("/hello", (req, res) => {
+  console.log(req);
+
+  // req { ip, query: {} }
+  // req.ip
+  // req.query.name
+
+  res.send("Hello " + req.query.name);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
