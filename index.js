216f8349a12f0fd9d50762498b0019a6fa36afbd
AlexPurcell <alextpurcell05@gmail.com>
2021-06-17 16:09:32 -0600

    DB and dotenv

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ var path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+import * as dotenv from "dotenv";
+dotenv.config();
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.engine(".html", require("ejs").__express);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 // GET http://localhost:3000/home/?name=alex
 app.get("/", (req, res) => {
