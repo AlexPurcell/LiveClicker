a39955d853acc78f903be7accbe0a8190a478120
Alex Purcell <alextpurcell05@gmail.com>
2021-06-16 23:20:25 -0600

    New Admin Panel

---
+++ public/admin.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", (event) => {
+  console.log("Content Loaded");
+
+  submit = document.getElementById("Submit");
+  submit.addEventListener("click", (event) => {
+    console.log("CLICKED");
+  });
+});
